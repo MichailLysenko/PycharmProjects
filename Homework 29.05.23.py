729b7d58f372ced83080ec9cf0c743956643fc82
Michail Lysenko <michail.lysenk@gmail.com>
2023-07-12 17:38:39 +0200

    Pierwsze sprawdzenie formuly obliczeniowej wartosci kredytu

--- Homework 29.05.23.py
+++ Homework 29.05.23.py
@@ -27,51 +27,63 @@ Listopad	-0,4041867176	8233,565935
 Grudzień	1,499708521	8064,439807
 Grudzień = 0.337779545187098 * Pozyczka
 '''
-'''
-Inflacja	Pożyczka
-Przed		12000
+
+#Inflacja	Pożyczka
+#Przed		12000
 #Styczeń
-inflacja_1 = 1.592824484	11845,92824
+inflacja_1 = 1.592824484	#11845,92824
 #Luty
-inflacja_2 = -0.4535091012	11671,0662
+inflacja_2 = -0.4535091012	#11671,0662
 #Marzec
-inflacja_3 = 2,324671717	11522,85337
+inflacja_3 = 2.324671717	#11522,85337
 #Kwiecień
-inflacja_4 = 1,261254407	11363,77154
+inflacja_4 = 1.261254407	#11363,77154
 #Maj
-inflacja_5 = 1,782526286	11209,06115
+inflacja_5 = 1.782526286	#11209,06115
 #Czerwiec
-inflacja_6 = 2,329384541	11058,84232
+inflacja_6 = 2.329384541	#11058,84232
 #Lipiec
-inflacja_7 = 1,502229842	10900,33353
+inflacja_7 = 1.502229842	#10900,33353
 #Sierpień
-inflacja_8 = 1,782526286	10743,77614
+inflacja_8 = 1.782526286	#10743,77614
 #Wrzesień
-inflacja_9 = 2,328848994	10591,4861
+inflacja_9 = 2.328848994	#10591,4861
 #Październik
-inflacja_10 = 0,6169213482	10423,40991
+inflacja_10 = 0.6169213482	#10423,40991
 #Listopad
-inflacja_11 = 2,352295886	10269,90089
+inflacja_11 = 2.352295886	#10269,90089
 #Grudzień
-inflacja_12 = 0,3377795452	10098,46645
-Styczeń	1,577035247	9936,983976
-Luty	-0,2927814426	9759,401966
-Marzec	2,48619659	9604,020297
-Kwiecień	0,2671103178	9430,168126
-Maj	1,417952672	9264,886489
-Czerwiec	1,054243267	9096,188242
-Lipiec	1,480520104	8930,151288
-Sierpnień	1,577035247	8764,212635
-Wrzesień	-0,07742069031	8585,557724
-Październik	1,165733399	8415,362011
-Listopad	-0,4041867176	8233,565935
-Grudzień	1,499708521	8064,439807
-'''
+inflacja_12 = 0.3377795452	#10098,46645
+#Styczeń
+inflacja_13 = 1.577035247	#9936,983976
+#Luty
+inflacja_14 = -0.2927814426	#9759,401966
+#Marzec
+inflacja_15 = 2.48619659	#9604,020297
+#Kwiecień
+inflacja_16 = 0.2671103178	#9430,168126
+#Maj
+inflacja_17 = 1.417952672	#9264,886489
+#Czerwiec
+inflacja_18 = 1.054243267	#9096,188242
+#Lipiec
+inflacja_19 = 1.480520104	#8930,151288
+#Sierpnień
+inflacja_20 = 1.577035247	#8764,212635
+#Wrzesień
+inflacja_21 = -0.07742069031	#8585,557724
+#Październik
+inflacja_22 = 1.165733399	#8415,362011
+#Listopad
+inflacja_23 = -0.4041867176	#8233,565935
+#Grudzień
+inflacja_24 = 1.499708521	#8064,439807
+
 oprocentowanie_kredytu = input('Podaj oprocentowanie kredytu: ')
 kwota_poczatkowa_kredytu = input('Podaj kwote poczatkowa kredytu: ')
 stala_wartosc_raty = input('Podaj stala wartosc raty: ')
 print(f'oprocentowanie kredytu to: {oprocentowanie_kredytu}, kwota poczatkowa kredytu to: {kwota_poczatkowa_kredytu}, stala wartosc raty to: {stala_wartosc_raty}')
-
+formula_do_pierwszego_obliczenia = (1 + ((inflacja_1+oprocentowanie_kredytu)/1200)) * kwota_poczatkowa_kredytu - stala_wartosc_raty
 
 '''
 tabela_inflacji_wartosci
