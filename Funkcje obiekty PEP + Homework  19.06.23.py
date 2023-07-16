2a01ff4342c84ece206e546c9dfde9c4d97180a4
Michail Lysenko <michail.lysenk@gmail.com>
2023-07-16 13:43:45 +0200

    Zmiana print na return

--- Funkcje obiekty PEP + Homework  19.06.23.py
+++ Funkcje obiekty PEP + Homework  19.06.23.py
@@ -1,21 +1,17 @@
+def check_whether_user_has_shortened_work_time(users_job):
+    return users_job in ['Policjant', 'Strazak', 'Zolnierz'] #zwroci tu wartosc albo true albo false
+
 def check_my_age(users_age, users_job=None):
     #Argument do funkcji (users_age). Tu mamy definicje, a nie wywolanie funkcji, dlatego jest ona podana na poczatku.
-    if users_job not in ['Policjant', 'Strazak', 'Zolnierz']:
+    if check_whether_user_has_shortened_work_time(users_job):
         if 18 < users_age:
-            print('Nie powinienes jeszcze pracowac')
+            return('Nie powinienes jeszcze pracowac')
         elif 18 < users_age < 65:
-            print('Jestes w stanie pracowac')
+            return('Jestes w stanie pracowac')
         else:
-            print('Jestes w wieku emerytalnym')
+            return('Jestes w wieku emerytalnym')
     else:
-        if 18 < users_age:
-            print('Nie powinienes jeszcze pracowac')
-        elif 18 < users_age < 45:
-            print('Jestes w stanie pracowac')
-        else:
-            print('Jestes w wieku emerytalnym')
-def check_whether_user_has_shortened_work_time(users_job):
-    return users_job in ['Policjant', 'Strazak', 'Zolnierz']
+        pass
 
 my_job = input('Podaj swoja prace: ')
 my_age = int(input('Podaj swoj wiek: '))
