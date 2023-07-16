fafe9af548927d16e4a27a53255ffdbfae30dc09
Michail Lysenko <michail.lysenk@gmail.com>
2023-07-17 00:13:07 +0200

    Stan tuz przed rozpoczeciem tworzenia klas

--- Funkcje obiekty PEP + Homework  19.06.23.py
+++ Funkcje obiekty PEP + Homework  19.06.23.py
@@ -1,26 +1,46 @@
+'''
 def check_whether_user_has_shortened_work_time(users_job):
     return users_job in ['Policjant', 'Strazak', 'Zolnierz'] #zwroci tu wartosc albo true albo false
 
-def check_my_age(users_age, users_job=None):
+def check_my_age(users_age, users_job='Programista', *args, *kwargs):
+    print(args)
+    for argument in args:
+        print(argument)
+        if argument > 3999:
+            print('Masz dobrze platna prace')
+    for key, in value in kwargs.items():
+        print(f"{key}: {value}")
     #Argument do funkcji (users_age). Tu mamy definicje, a nie wywolanie funkcji, dlatego jest ona podana na poczatku.
     if check_whether_user_has_shortened_work_time(users_job):
-        if 18 < users_age:
+        if 18 > users_age:
             return('Nie powinienes jeszcze pracowac')
         elif 18 < users_age < 65:
             return('Jestes w stanie pracowac')
         else:
             return('Jestes w wieku emerytalnym')
     else:
-        pass
+        if users_job == 'Nauczyciel':
+            return 'Jestes nauczycielem'
+        return 'Nie weszlismy w zaden warunek'
 
 my_job = input('Podaj swoja prace: ')
 my_age = int(input('Podaj swoj wiek: '))
 
 if my_job == 'Konserwator':
-    check_my_age(my_age)
+    displayed_text = check_my_age(my_age, my_job, pensja_konserwatora=4000)
 elif my_job == 'Programista':
-    check_my_age(my_age)
+    displayed_text = check_my_age(my_age, liczba_projektow=24)
 elif my_job == 'Nauczyciel':
-    check_my_age(my_age)
+    displayed_text = check_my_age(my_age, my_job)
 else:
-    check_my_age(my_age)
+    displayed_text = check_my_age(my_age, users_job=my_job)
+
+print(displayed_text)
+
+def check_my_grades(*args):
+    for grade in args:
+        if grade == 1:
+            print('Dostales jedynke')
+check_my_grades(1,2,3,1,5,6,3)
+'''
+#Obiektowosc. Klasy - to polaczenie zmiennych i funkcji
