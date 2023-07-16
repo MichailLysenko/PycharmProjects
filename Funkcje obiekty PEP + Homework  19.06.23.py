5bb7f4b88a63ede872dc2e116b89ccca6c5f13a9
Michail Lysenko <michail.lysenk@gmail.com>
2023-07-16 13:39:14 +0200

    Stan programu przed usupelnieniem definicji "check_whether_user_has_shortened_work_time"

--- Funkcje obiekty PEP + Homework  19.06.23.py
+++ Funkcje obiekty PEP + Homework  19.06.23.py
@@ -1,32 +1,30 @@
+def check_my_age(users_age, users_job=None):
+    #Argument do funkcji (users_age). Tu mamy definicje, a nie wywolanie funkcji, dlatego jest ona podana na poczatku.
+    if users_job not in ['Policjant', 'Strazak', 'Zolnierz']:
+        if 18 < users_age:
+            print('Nie powinienes jeszcze pracowac')
+        elif 18 < users_age < 65:
+            print('Jestes w stanie pracowac')
+        else:
+            print('Jestes w wieku emerytalnym')
+    else:
+        if 18 < users_age:
+            print('Nie powinienes jeszcze pracowac')
+        elif 18 < users_age < 45:
+            print('Jestes w stanie pracowac')
+        else:
+            print('Jestes w wieku emerytalnym')
+def check_whether_user_has_shortened_work_time(users_job):
+    return users_job in ['Policjant', 'Strazak', 'Zolnierz']
+
 my_job = input('Podaj swoja prace: ')
 my_age = int(input('Podaj swoj wiek: '))
-def check_my_age():
 
 if my_job == 'Konserwator':
-    if my_age < 18:
-        print('Nie powinienes jeszcze pracowac')
-    elif 18 < my_age < 65:
-        print('Jestes w stanie pracowac')
-    else:
-        print('Jestes w wieku emerytalnym')
+    check_my_age(my_age)
 elif my_job == 'Programista':
-    if my_age < 18:
-        print('Nie powinienes jeszcze pracowac')
-    elif 18 < my_age < 65:
-        print('Jestes w stanie pracowac')
-    else:
-        print('Jestes w wieku emerytalnym')
-if my_job == 'Nauczyciel':
-    if my_age < 18:
-        print('Nie powinienes jeszcze pracowac')
-    elif 18 < my_age < 65:
-        print('Jestes w stanie pracowac')
-    else:
-        print('Jestes w wieku emerytalnym')
+    check_my_age(my_age)
+elif my_job == 'Nauczyciel':
+    check_my_age(my_age)
 else:
-    if my_age < 18:
-        print('Nie powinienes jeszcze pracowac')
-    elif 18 < my_age < 65:
-        print('Jestes w stanie pracowac')
-    else:
-        print('Jestes w wieku emerytalnym')
+    check_my_age(my_age)
