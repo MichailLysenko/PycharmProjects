d81d81668c40e5c4a0e2d3395f1ffc27ef3887e5
Michail Lysenko <michail.lysenk@gmail.com>
2024-02-12 22:02:54 +0100

    Dodanie w linii 124 metody ".get()".

--- Homework 12.06.23.py
+++ Homework 12.06.23.py
@@ -1,6 +1,17 @@
+with open ("saldo.txt") as file_stream:
+    written_text_first = file_stream.read()
+    print(written_text_first)
+with open ("Magazyn.txt") as file_stream:
+    written_text_second = file_stream.read()
+    print(written_text_second)
+
 # Magazyn to jest lista slownikow
 # Magazyn_1 to jest slownik z klucza (nazwa) slownikow
 saldo = 8000.0
+
+with open("saldo.txt", mode="w") as file_stream:
+    file_stream.writelines(str(saldo) + "\n")
+
 magazyn = {
     'rower': {
         'ilosc': 2,
@@ -20,6 +31,8 @@ new_product = None
 new_amount = None
 new_saldo = None
 product = None
+
+
 initial_message = 'Witaj w Twoim magazynie. Lista dostepnych komend to:\n' \
                   ' 1. Saldo\n 2. Sprzedaz\n 3. Zakup\n 4. Konto\n 5. Lista\n 6. Magazyn\n 7. Przeglad\n 8. Koniec'
 
@@ -29,11 +42,15 @@ while not end_program:
     operation = input('Podaj operacje ktora chcesz wykonac: ')
     # TODO: Przerobic ponizszy if na match_case
     if operation == '1':
-        amount = float(input('Podaj kwote, ktora chcesz dodac lub ujac z konta: '))
+        amount = float(input(f'Podaj kwote, ktora chcesz dodac lub ujac z konta. Jeżęli chcesz ujmować, proszę podaj kwotę ujemną: '))
         saldo += amount
         # Jezeli chcemy odjac, to dajemy kwote ujemna
         # TODO poprosic jeszcze o rodzaj operacji: czy dodajemy, czy odejmujemy
-        history.append(f'Wykonano instrukcje saldo, zasilono {amount}')
+        print(f"Konto zmieniono o kwote {amount}, stan konta wynosi {saldo}")
+        history.append(f'Wykonano instrukcje saldo, zasilono lub ujęto {amount}')
+        print(history)
+        with open("saldo.txt", mode="w") as file_stream:
+            file_stream.writelines(str(saldo) + "\n")
     if operation == '2':
         print(magazyn)
         product = str(input('Podaj nazwe produktu: '))
@@ -59,22 +76,18 @@ while not end_program:
         price = float(input('Podaj cene produktu: '))
         product_found = False
         # Najpierw sprawdzmy, czy mamy towar
-
         for item, item_details in magazyn.items():  # Bez items byliby tylko kluczy.
             if product == item:
                 item_details['ilosc'] += amount
                 saldo -= (item_details['cena'] * amount) and saldo > 0
                 product_found = True
-
                 history.append(f'Zakupiono {product} w ilosci {amount}')
                 break
         if not product_found:
-
             new_product = input(
                 f'Jeszcze nie mamy takiego produktu na magazynie.\nProsze ponownie podac nazwe produktu -\nzostanie on wpisany do naszej bazy danych: ')
             new_amount = int(input(f'Prosze podac ilosc produktu: '))
             price_to_pay = price * new_amount
-
                 #tutaj musisz stworzyc nowy produkt tylko, ze bez fora
             if saldo - price_to_pay > 0:
                 print(f'Zakupiono {new_product} w ilosci {new_amount} sztuk')
@@ -82,13 +95,10 @@ while not end_program:
                 magazyn[new_product]['ilosc'] = new_amount
                 magazyn[new_product]['cena'] = price
                 new_saldo = saldo - price_to_pay
-
-
                 product_found = False
                 history.append(f'Zakupiono {new_product} w ilosci {new_amount} sztuk')
             else:
                 print("Niestety nie stać Cię na zakup tego produktu")
-
 
         # program pobiera nazwe produktu, cene oraz liczbe sztuk.
     # produkt zostaje dodany do magazynu, jesli go nie bylo
@@ -104,22 +114,28 @@ while not end_program:
             print(new_saldo)
         else:
             print('Konto magazynu jest puste')
-
     if operation == '5':  # program wyswietla calkowity stan magazynu wraz z cenami produktow i ich iloscia
         print(magazyn)
-
     if operation == '6':
         searched_product = str(input("Prosze podac nazwe produktu: "))
-        for key in magazyn:  # Bez items beda tylko kluczy.
-            if searched_product == key:
-                print(f"W magazynie znaleziono {searched_product}")
-                break
-            else:
-                print(f"Nie znaleziono {searched_product}")
+        #if magazyn.get(searched_product) == searched_product: todo: to sie nie powiodlo,
+        # todo:.get() sprawdzało, czy klucz istnieje, ale potem porównywałem wynik tej metody z searched_product,
+        #  co jest stringiem, a nie słownikiem. Wartość zwracana przez .get() to wartość dla danego klucza, a nie sam klucz.
+        if magazyn.get(searched_product): # zrobiłem to w ten sposób
+            print("W magazynie znaleziono " + searched_product)
+            history.append(f'Znaleziono {searched_product}')
+            pass
+            with open("magazyn.txt", mode="w") as file_stream:
+                file_stream.writelines(f"W magazynie znaleziono " + searched_product + "\n")
+        else:
+            print(f"Nie znaleziono " + searched_product)
+            history.append(f'Nie znaleziono {searched_product}')
+            with open("magazyn.txt", mode="w") as file_stream:
+                file_stream.writelines(f"W magazynie nie znaleziono " + searched_product + "\n")
 
         # Magazyn - Program wyświetla stan magazynu dla konkretnego produktu. Należy podać jego nazwę.
         # dane wejsciowe - nazwa, wyszukiwac po nazwie
-        pass
+
     # TODO zrobic For, porownac, czy mamy produkt.
     if operation == '7':
         # TODO jak value bedzie wieksze od naszej listy to wyprintowac dlugosc listy instrukcja len()
