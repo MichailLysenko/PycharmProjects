20e3794c7ab41ddecddd02574f6923cd1abeb003
Michail Lysenko <michail.lysenk@gmail.com>
2024-02-12 17:57:37 +0100

    Ostatnie zmiany 12.02. Usuniecie zbednych liniii kodu.

--- Funkcje obiekty PEP + Homework  19.06.23.py
+++ Funkcje obiekty PEP + Homework  19.06.23.py
@@ -78,7 +78,7 @@ check_my_grades(1,2,3,1,5,6,3)
 # Po wykonaniu każdej z opcji (oprócz "koniec") wyświetla to menu ponownie.
 # Polecenie "klasa" - Należy pobrać klasę, którą chcemy wyświetlić (np. "3C")
 # program ma wypisać wszystkich uczniów, którzy należą do tej klasy,
-# a także wychowawcę tejże klasy.
+# todo a także wychowawcę tejże klasy.
 # Polecenie "uczeń" - Należy pobrać imię i nazwisko uczenia, program ma wypisać wszystkie
 # lekcje, które ma uczeń a także nauczycieli, którzy je prowadzą.
 # Polecenie "nauczyciel" - Należy pobrać imię i nazwisko nauczyciela, program ma wypisać
@@ -110,11 +110,18 @@ class Teacher:
         self.subject = subject
         self.class_name = class_name
 
+    def __repr__(self):
+        return f"Imie = {self.name}, Nazwisko = {self.surname}, Przedmiot = {self.subject}, Klasa = {self.class_name}"
+
+
 class Educator:
     def __init__(self, name, surname, class_name):
         self.name = name
         self.surname = surname
         self.class_name = class_name
+
+    def __repr__(self):
+        return f"Imie = {self.name}, Nazwisko = {self.surname}, Klasa = {self.class_name}"
 
   #  def __repr__(self):
     #    return f"Imie = {self.name}, Nazwisko = {self.surname}, Przedmiót = {self.subject}, Klasy = {self.class_name}"
@@ -122,6 +129,44 @@ class Educator:
 
 # TODO W domu zrobić klasy dla nauczyciela i wychowawcy i zmienili je w naszej szkole
 
+students = [
+    Student(name="Jan", surname="Nowak", class_name="2a"),
+    Student(name="Piotr", surname="Kamienny", class_name="3a"),
+    Student(name="Marlena", surname="Morska", class_name="7a"),
+    Student(name="Konstanty", surname="Wysocki", class_name="5b"),
+    Student(name="Stefan", surname="Brum", class_name="4b"),
+    Student(name="Paweł", surname="Pacek", class_name="4a"),
+    Student(name="Michał", surname="Krynicki", class_name="5b"),
+    Student(name="Hanna", surname="Retecka", class_name="8a"),
+    Student(name="Joanna", surname="Katarska", class_name="5b"),
+    Student(name="Tomasz", surname="Prędky", class_name="6b"),
+    Student(name="Jan", surname="Puszek", class_name="6a")
+
+]
+
+teachers = [
+    Teacher(name='Andrzej', surname='Iwan', subject='WF', class_name=['1a', '2a']),
+    Teacher(name='Paweł', surname='Strzelczyk', subject='Fizyka', class_name=['7a', '8a']),
+    Teacher(name='Elżbieta', surname='Kowalska', subject='Geografia', class_name=['6a', '6b']),
+    Teacher(name='Anna', surname='Kamińska', subject='Język Angielski', class_name=['1a', '1b', '1c', '2a', '2b', '2c', '3a', '3b']),
+    Teacher(name='Helena', surname='Prosta', subject='Język Polski', class_name=['5a', '6a', '5b', '6b']),
+    Teacher(name='Stanisław', surname='Kauer', subject='Plastyka', class_name=['7a', '8a']),
+    Teacher(name='Mikołaj', surname='Wawrzyński', subject='Matematyka', class_name=['4a', '5a']),
+    Teacher(name='Maria', surname='Wierna', subject='Biologia', class_name=['7a', '7b'])
+]
+
+educators = [
+    Educator(name= "Włodzimierz", surname="Stefanczyk", class_name=["1a"]),
+    Educator(name= "Andrzej", surname="Iwan", class_name=["2a"]),
+    Educator(name= "Paweł", surname="Strzelczyk", class_name=["3a"]),
+    Educator(name= "Elżbieta", surname="Kowalska", class_name=["1b"]),
+    Educator(name= "Anna", surname="Kamińska", class_name=["2b"]),
+    Educator(name= "Helena", surname="Prosta", class_name=["3b"]),
+    Educator(name= "Stanisław", surname="Kauer", class_name=["4a"]),
+    Educator(name= "Mikołaj", surname="Wawrzyński", class_name=["4b"]),
+    Educator(name= "Maria", surname="Wierna", class_name=["5a"])
+]
+"""
 our_school = {
     "klasy": {
         "1a": {
@@ -155,7 +200,7 @@ def create_new_grade(grade):
 def create_student_in_existing_class(name, surname, class_name):
     our_school["klasy"][class_name]["uczniowie"].append(Student(name=name, surname=surname, class_name=class_name))
 
-def create_teacher_in_existing_class(name,surname,subject, class_name):
+def create_teacher_in_existing_class(name,surname,subject,class_name):
     our_school ["nauczyciele"][Teacher].append(Teacher(name=name, surname=surname, subject=subject, class_name=class_name))
 
 def create_educator_in_existing_class(name,surname,class_name):
@@ -166,7 +211,7 @@ def create_new_student(name, surname, class_name):
         "uczniowie": []
     }
 def create_new_student(name, surname, class_name):
-    our_school.get("klasy").get(class_name)
+    class_exists = our_school.get("klasy").get(class_name)
     if not class_exists:
         create_new_class_name(class_name)
     create_student_in_existing_class(name, surname, class_name)
@@ -214,7 +259,7 @@ def find_student_by_name(name, surname):
                     our_text += f'Nauczyciel: {teacher.name} {teacher.surname} z przedmiotem {teacher.subject}\n'
                 return our_text
     return 'Niestety Twoja klasa nie ma żadnych zajęć'
-
+"""
 
 initial_menu = "Witaj w swojej szkole! Podaj proszę co chcesz zrobić:\n 1.Utwórz\n 2.Zarządzaj\n 3.Koniec\n"
 
@@ -230,49 +275,99 @@ while not finish_program:
             name = input("Podaj imię ucznia: ")
             surname = input("Podaj nazwisko ucznia: ")
             class_name = input("Podaj klasę ucznia: ")
-            create_new_student(name, surname, class_name)
-            print(our_school)
+            #create_new_student(name, surname, class_name)
+            student = Student(name=name, surname=surname, class_name=class_name)
+            students.append(student)
+            print(student)
+            print("Nowy uczeń został dodany do systemu")
+            #print(our_school)
         elif create_input == "2":
             name = input("Podaj imie nauczyciela: ")
-            surname = input ("Podaj nazwisko nauczyciela:")
-            subject = input ("Podaj przedmiot nauczyciela: ")
-            class_name = input ("Podaj nazwe klasy nauczyciela: ")
-            create_new_teacher(name, surname, subject, class_name)
-            print(our_school)
+            surname = input("Podaj nazwisko nauczyciela:")
+            subject = input("Podaj przedmiot nauczyciela: ")
+            class_name = input("Podaj nazwe klasy nauczyciela: ")
+            teacher = Teacher(name=name, surname=surname, subject=subject, class_name=class_name)
+            teachers.append(teacher)
+            print(teacher) #todo Tutaj przy wykonaniu "run" wychodzi wartosc numeryczna z nazwa galęzi(?) - chociaż przypadek identyczny z uczniami
+            print("Nowy nauczyciel został dodany do systemu")
+            #create_new_teacher(name, surname, subject, class_name)
+            #print(our_school)
         elif create_input == "3":
             name = input ("Podaj imie wychowawcy: ")
             surname = input("Podaj nazwisko wychowawcy: ")
             class_name = input("Podaj klase wychowawcy: ")
-            create_new_educator(name, surname, class_name)
-            print(our_school)
+            educator = Educator(name=name, surname=surname, class_name=class_name)
+            educators.append(educator)
+            print(educator)
+            print("Nowy wychowawca został dodany do systemu")
+            #create_new_educator(name, surname, class_name)
+            #print(our_school)
         elif create_input == "4":
             print("Koniec działania programu")
 
     elif main_guess == "2":
         manage_input = int(input(manage_menu))
-        if manage_input == "1":
+        if manage_input == 1:
             class_name = input("Podaj nazwę klasy: ")
-            text_to_display = find_grade_by_class_number(class_name)
-            print(text_to_display)
-        elif manage_input == "2":
+            #1 Dziala na podstawie listy uczniow. Czy tu trzeba dodac wyszukiwanie po nauczycielach i wychowawcach?
+            class_found = False
+            for name_of_class in students:
+                if name_of_class.class_name == class_name:
+                    print(name_of_class)
+                    class_found = True
+            if not class_found:
+                print("Nie znalieziono takiej klasy na podstawie przeglądu listy uczniów")
+            class_found = False
+            for name_of_class_by_edu in educators: #todo Czemu nie mozna wyciagnac danych o klasie z listy wychowawców?... Aaa - bo dla listy trzeba podac [wartosc] :D
+                if name_of_class_by_edu.class_name == class_name or name_of_class_by_edu.class_name == [class_name]:
+                    print(name_of_class_by_edu)
+                    class_found = True
+            if not class_found:
+                print("Nie znalieziono takiej klasy na podstawie przeglądu listy wychowawców")
+            #text_to_display = find_grade_by_class_number(class_name)
+            #print(text_to_display)
+        elif manage_input == 2:
             student_name = input('Podaj imię ucznia: ')
             student_surname = input('Podaj nazwisko ucznia: ')
-            text = find_student_by_name(student_name, student_surname)
-            print(text)
-        elif manage_input == "3":
+            student_found = False
+            # Specjalnie zmienilem indeks w forze na podobna nazwe do studenta, zeby uswiadomic, ze nazwa jest obojetna,
+            # ale zeby tez nie stracic nawiazania do tematu. Bo w poprzednim przypadku "Student" nastapilo nadpisanie klasy.
+            for pupil in students:
+                if pupil.name == student_name and pupil.surname == student_surname:
+                    print(pupil)
+                    student_found = True
+            if not student_found:
+                print("Nie znaleziono takiego ucznia")
+            #text = find_student_by_name(student_name, student_surname)
+            #print(text)
+        elif manage_input == 3:
             teachers_name = input('Podaj imię nauczyciela: ')
             teachers_surname = input('Podaj nazwisko nauczyciela: ')
             class_name = input("Podaj nazwę klasy: ")
-            text_to_display = find_class_teachers(our_school)
-            print(text_to_display)
-        elif manage_input == "4":
+            teacher_found = False
+            for lector in teachers:
+                if lector.name == teachers_name and lector.surname == teachers_surname or lector.class_name == [class_name]:
+                    print(lector)
+                    teacher_found = True
+            if not teacher_found:
+                print("Nie znaleziono takiego nauczyciela")
+            #text_to_display = find_class_teachers(our_school)
+            #print(text_to_display)
+        elif manage_input == 4:
             educator_name = input('Podaj imię wychowawcy: ')
             educator_surname = input('Podaj nazwisko wychowawcy: ')
-            text = find_class_educators(our_school)
-            print(text)
-        elif manage_input == "5":
+            educator_found = False
+            for behaviorist in educators:
+                if behaviorist.name == educator_name and behaviorist.surname == educator_surname:
+                    print(behaviorist)
+                    educator_found = True
+            if not educator_found:
+                print("Nie znaleziono takiego wychowawcy") #todo czemu zawsze wychodzi, ze nie ma takiego wychowawcy, nawet jak on jest? Załatwiono
+            #text = find_class_educators(our_school)
+            #print(text)
+        elif manage_input == 5:
             print("Koniec dzialania programu")
     elif main_guess == "3":
-        print("Koniec dialania programu")
+        print("Koniec dzialania programu")
 
 
