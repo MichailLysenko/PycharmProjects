a1da4064bb4affea111cbae03a1f5170b1bed165
Michail Lysenko <michail.lysenk@gmail.com>
2024-05-27 20:07:36 +0200

    Stan po konsultacji - opracowanie kodu, zmiana go na funkcje i wypracowanie szablonu dla dalszego postepowania.

--- Homework podstawowe interfejsy2.py
+++ Homework podstawowe interfejsy2.py
@@ -2,61 +2,78 @@ import json
 import requests
 import geopy
 
-
-# 1 Odczytywanie pliku
-with open("new_weather_forecast2.json", mode="r") as file_stream:
-    data = json.load(file_stream)
-print(data)
 should_overwrite_file = False
 
-# 2 Pobieranie danych od użytkownika
-city = input("Podaj nazwę miasta dla którego chcesz sprawdzić pogodę: ")
-searched_date = input("Podaj datę dla której chcesz sprawdzić pogodę. Proszę użyć formatu 'rok-miesiąc-dzień': ")
+class WeatherForecast:
 
-# 3 Pobieranie wartosci
-if searched_date in data:
-    value = data[searched_date]
-else:
-    # URL do API:
-    # https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=rain&daily=rain_sum&timezone=Europe%2FLondon&start_date={searched_date}&end_date={searched_date}
-    # W URL należy uzupełnić parametry: latitude, longitude oraz searched_date
+    def main(self):
+        data = self.read_file()
+        city, searched_date = self.get_users_data()
+        should_overwrite_file = False
+        if searched_date in data:
+            value = data[searched_date]
+        else:
+            get_value = self.get_value()
+            data[searched_date] = value
+            should_overwrite_file = True
 
-    geolocator = geopy.Nominatim(user_agent="Homework_venv_pip_dict")
+    def read_file(self):
+        with open("new_weather_forecast2.json", mode="r") as file_stream:
+            data = json.load(file_stream)
+        print(data)
+        return data
 
-    location = geolocator.geocode(city)
-    print(location)
-    print(dir(location))
-    print(location.latitude)
-    print(location.longitude)
+    def get_users_data(self):
+        city = input("Podaj nazwę miasta dla którego chcesz sprawdzić pogodę: ")
+        searched_date = input(
+            "Podaj datę dla której chcesz sprawdzić pogodę. Proszę użyć formatu 'rok-miesiąc-dzień': ")
+        return city, searched_date
 
-    latitude = location.latitude
-    longitude = location.longitude
-    # 2. Budowa linku do zapytania
-    URL = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=rain&daily=rain_sum&timezone=Europe%2FLondon&start_date={searched_date}&end_date={searched_date}"
-    # 3. Wysłanie zapytania
-    response = requests.get(URL) # Wysylamy zapytanie do API
-    print(response.json()["daily"]["rain_sum"])
-    #
-    weather_forecast_result = response.json()["daily"]["rain_sum"]
-    value = weather_forecast_result[0]
-    data[searched_date] = value
-    should_overwrite_file = True
+    def get_value(self):
 
-# 4.1 Weryfikacja
-if value > 0.0:
-    result = "będzie padać"
-elif value == 0.0:
-    result = "nie będzie padać"
-else:
-    result = "nie mogę wskazać prognozu pogody"
-#     # Przepisac wynik do zmiennej
+            # URL do API:
+            # https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=rain&daily=rain_sum&timezone=Europe%2FLondon&start_date={searched_date}&end_date={searched_date}
+            # W URL należy uzupełnić parametry: latitude, longitude oraz searched_date
 
-# 4.2 Wyswietlenie wyniku
-print(f"W miejscowosci {city} w dniu {searched_date} {result}")
-# new_data = json.dump(new_weather_forecast2, file_stream)
+            geolocator = geopy.Nominatim(user_agent="Homework_venv_pip_dict")
 
-# 5 Warunek nadpsisywania wartosci
+            location = geolocator.geocode(city)
+            print(location)
+            print(dir(location))
+            print(location.latitude)
+            print(location.longitude)
+
+            latitude = location.latitude
+            longitude = location.longitude
+            # 2. Budowa linku do zapytania
+            URL = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=rain&daily=rain_sum&timezone=Europe%2FLondon&start_date={searched_date}&end_date={searched_date}"
+            # 3. Wysłanie zapytania
+            response = requests.get(URL)  # Wysylamy zapytanie do API
+            print(response.json()["daily"]["rain_sum"])
+            #
+            weather_forecast_result = response.json()["daily"]["rain_sum"]
+            value = weather_forecast_result[0]
+            data[searched_date] = value
+            should_overwrite_file = True
+            return value
+
+    def is_going_to_rain(self, value):
+        if value > 0.0:
+            result = "będzie padać"
+        elif value == 0.0:
+            result = "nie będzie padać"
+        else:
+            result = "nie mogę wskazać prognozu pogody"
+        return result
+
+    def print_result(self):
+        print(f"W miejscowosci {city} w dniu {searched_date} {result}")
+
+    def overwrite_file(self):
+        with open("new_weather_forecast2.json", mode='w') as file_stream:
+            json.dump(data, file_stream)
+
+
 if should_overwrite_file:
-    with open("new_weather_forecast2.json", mode='w') as file_stream:
-        json.dump(data, file_stream)
+
 
